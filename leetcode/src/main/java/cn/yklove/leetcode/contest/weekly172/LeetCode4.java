1e53c1edfabe08fd5e3a844158bc51a7b454c1bf
qinggeng <1033025319@qq.com>
2020-01-22 20:23:04 +0800

    周赛

--- leetcode/src/main/java/cn/yklove/leetcode/contest/weekly172/LeetCode4.java
+++ leetcode/src/main/java/cn/yklove/leetcode/contest/weekly172/LeetCode4.java
@@ -1,78 +1,60 @@
 package cn.yklove.leetcode.contest.weekly172;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author qinggeng
  */
 public class LeetCode4 {
 
-    private int[] ranges;
-    private int n;
 
     public int minTaps(int n, int[] ranges) {
-        this.ranges = ranges;
-        this.n = n;
-        Info f = f(null, 0, 0);
-        Info f1 = f(null, 1, 0);
-        if (f == null && f1 == null) {
-            return -1;
+        List<Info> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(new Info(i - ranges[i], i + ranges[i]));
         }
-        if (f == null) {
-            return f1.count;
-        }
-        if (f1 == null) {
-            return f.count;
-        }
-        return Math.min(f.count, f1.count);
-    }
-
-    private Info f(Info info, int off, int i) {
-        Info t = new Info();
-        if (off == 1) {
-            if (info == null) {
-                t.start = i - ranges[i];
-                t.end = i + ranges[i];
-                t.count++;
-            } else {
-                t = info;
-                t.start = Math.min(t.start, i - ranges[i]);
-                t.end = Math.max(t.end, i + ranges[i]);
-                t.count++;
+        list.sort(Comparator.comparingInt(o -> o.left));
+        int current = 0;
+        int max = Integer.MIN_VALUE;
+        int maxIndex = -1;
+        int count = 0;
+        while (current < n) {
+            boolean key = true;
+            count++;
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).left <= current) {
+                    if (list.get(i).right > max) {
+                        maxIndex = i;
+                        max = list.get(i).right;
+                        key = false;
+                    }
+                }
             }
-        } else {
-            t = info;
+            if (key) {
+                return -1;
+            }
+            current = list.get(maxIndex).right;
         }
-        if (i >= n) {
-            return t;
-        }
-        Info f = f(t, 0, i + 1);
-        Info f1 = f(t, 1, i + 1);
-        if (f == null && f1 == null) {
-            return null;
-        }
-        if (f == null) {
-            return f1;
-        }
-        if (f1 == null) {
-            return f;
-        }
-        if (f.count > f1.count) {
-            return f1;
-        } else if(f.count == f1.count){
-            return f;
-        } else{
-            return f;
-        }
+        return count;
     }
 
     static class Info {
-        int start;
-        int end;
-        int count;
+        public Info(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        int left;
+        int right;
     }
 
-
     public static void main(String[] args) {
-//        System.out.println(new LeetCode4().minTaps(5,new int[]{3,4,1,1,0,0}));
-        System.out.println(new LeetCode4().minTaps(1, new int[]{0, 0}));
+        System.out.println(new LeetCode4().minTaps(5, new int[]{3, 4, 1, 1, 0, 0}));
+        System.out.println(new LeetCode4().minTaps(1, new int[]{0, 0, 0, 0}));
+        System.out.println(new LeetCode4().minTaps(7, new int[]{1, 2, 1, 0, 2, 1, 0, 1}));
+        System.out.println(new LeetCode4().minTaps(8, new int[]{4,0,0,0,0,0,0,0,4}));
+        System.out.println(new LeetCode4().minTaps(8, new int[]{4,0,0,0,4,0,0,0,4}));
     }
 }
